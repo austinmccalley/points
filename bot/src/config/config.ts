d3506305c6aea4470f63af6bcc067e48132c9435
LXing <31298876+FlyOrBoom@users.noreply.github.com>
2020-05-19 23:56:47 -0700

    added thx, rm others since only need to contain keyword

--- bot/src/config/config.ts
+++ bot/src/config/config.ts
@@ -36,5 +36,5 @@ export const config: BotConfig = {
     '711254277141561375': 5,
     'thanks': 10,
   },
-  thanksKeywords: ["thanks", "thank", "thunk", "kudos"],
+  thanksKeywords: ["thank","thunk","thx","kudo"],
 };
